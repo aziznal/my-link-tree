af0e5be6811ca442d66ea8282bf1c929ff25fc6c
Aziz <aziznal.dev@gmail.com>
2024-01-11 01:55:53 +0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 const fontFamily = Roboto({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en" className="min-h-full">
       <body className={cn(fontFamily.className, "h-full bg-rose-100")}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
