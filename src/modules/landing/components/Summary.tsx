c1e1867fc7aeb4c93fee277de7077162414bc429
aziznal <aziznal.dev@gmail.com>
2026-02-10 07:58:43 +0300

    update summary section

--- src/modules/landing/components/Summary.tsx
+++ src/modules/landing/components/Summary.tsx
@@ -9,18 +9,20 @@ export function Summary() {
     <div>
       <SectionTitle>A BIT ABOUT ME</SectionTitle>
 
-      <div className="text-muted-foreground text-balance max-w-[70ch] space-y-6">
+      <div className="max-w-[70ch] box-content space-y-6 border-3 p-6 font-mono lowercase">
+        <p>Hi. I'm Aziz.</p>
+
         <p>
           I've been a developer for the last {elapsedYears} years. I've worked
           as a{' '}
-          <span className="text-[22px] text-glow-sm text-glow-accent font-lowres">
-            Senior
+          <span className="text-[22px] text-glow-sm text-shadow-primary font-lowres">
+            Senior Full-Stack web developer
           </span>
-          ™ Full-Stack developer and Team Lead for the last 3 years. Check out
+          ™  and <span className="text-[22px] text-glow-sm text-shadow-primary font-lowres">Team Lead</span> for the last 3 years. Check out
           my CV for details
         </p>
 
-        <p>I enjoy making cool components and animations in my spare time.</p>
+        <p>I enjoy reading & making cool components and animations in my spare time.</p>
       </div>
     </div>
   )
