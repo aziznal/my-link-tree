ece94cd66530d9ec52b20c8650ac78e93db80533
aziznal <aziznal.dev@gmail.com>
2026-02-10 07:30:36 +0300

    add favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,7 +15,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: "Aziz Nal - Web Dev",
+        title: "Aziz Nal",
         description: 'A senior developer who does web',
       },
     ],
