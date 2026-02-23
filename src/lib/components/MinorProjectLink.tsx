5e1aaa47fc9f10401c461405716be862b6a2ffa4
aziznal <aziznal.dev@gmail.com>
2026-02-24 01:15:30 +0300

    fix project link wrapping

--- src/lib/components/MinorProjectLink.tsx
+++ src/lib/components/MinorProjectLink.tsx
@@ -10,32 +10,34 @@ export function MinorProjectLink({
   text: string
 }) {
   return (
-    <div className="font-lowres text-[1.675rem] space-x-2 leading-none items-center hover:bg-foreground/10">
-      <PixelatedChevron className="group-hover:fill-background fill-foreground" />
+    <div className="font-lowres flex text-[1.675rem] gap-2 leading-none hover:bg-foreground/10">
+      <PixelatedChevron className="group-hover:fill-background fill-foreground shrink-0 mt-1" />
 
-      <span>{text}</span>
+      <div className="space-x-2 inline">
+        <span>{text}</span>
 
-      {siteUrl !== undefined && (
-        <a
-          className="hover:text-background hover:bg-foreground h-fit ml-2 text-[1.625rem] text-foreground/40"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={siteUrl}
-        >
-          [site]
-        </a>
-      )}
+        {siteUrl !== undefined && (
+          <a
+            className="hover:text-background hover:bg-foreground h-fit ml-2 text-[1.625rem] text-foreground/40"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={siteUrl}
+          >
+            [site]
+          </a>
+        )}
 
-      {githubUrl !== undefined && (
-        <a
-          className="hover:text-background hover:bg-foreground h-fit ml-2 text-[1.625rem] text-foreground/40"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={githubUrl}
-        >
-          [github]
-        </a>
-      )}
+        {githubUrl !== undefined && (
+          <a
+            className="hover:text-background hover:bg-foreground h-fit ml-2 text-[1.625rem] text-foreground/40"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={githubUrl}
+          >
+            [github]
+          </a>
+        )}
+      </div>
     </div>
   )
 }
